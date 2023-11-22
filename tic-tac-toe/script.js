1268cba5711a53708eeb5bf422eeebcd81bccae3
hwan-kr <fabronjeon@naver.com>
2023-11-23 01:43:30 +0900

    If click Edit-button, Popup window and Popup-background's display turned none to block

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,4 +1,4 @@
-// game start button
+// game start
 const newGameButton = document.getElementById('start-game');
 const gameBoardId = document.getElementById('game-board');
 
@@ -6,3 +6,18 @@ function createNewGameBoard() {
     gameBoardId.style.display = 'block';
 }
 newGameButton.addEventListener('click', createNewGameBoard);
+
+// edit button
+const editButton = document.querySelectorAll('.edit-button');
+
+function openPopupWindow() {
+    const grayOverlay = document.getElementById('overlay');
+    const nameEditPopup = document.getElementById('name-edit-popup');
+    grayOverlay.style.display = 'block';
+    nameEditPopup.style.display = 'block';
+}
+
+// querySelectorAll 은 배열처럼 (배열은 아님) 요소를 출력 하기에 반복해서 입력해줘야함
+for (const button of editButton) {
+    button.addEventListener('click', openPopupWindow);
+}
