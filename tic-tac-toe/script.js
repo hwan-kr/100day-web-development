1d6d65546f760ef4c64fdf2886ccb31883fad199
hwan-kr <fabronjeon@naver.com>
2023-11-24 01:55:55 +0900

    Change Edit-button selector class to id. Because of change name issue

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -8,7 +8,8 @@ function createNewGameBoard() {
 newGameButton.addEventListener('click', createNewGameBoard);
 
 // edit button
-const editButton = document.querySelectorAll('.edit-button');
+const firstEditButton = document.getElementById('first-edit-button');
+const secondEditButton = document.getElementById('second-edit-button');
 
 function openPopupWindow() {
     const grayOverlay = document.getElementById('overlay');
@@ -18,6 +19,21 @@ function openPopupWindow() {
 }
 
 // querySelectorAll 은 배열처럼 (배열은 아님) 요소를 출력 하기에 반복해서 입력해줘야함
-for (const button of editButton) {
-    button.addEventListener('click', openPopupWindow);
+firstEditButton.addEventListener('click', openPopupWindow);
+secondEditButton.addEventListener('click', openPopupWindow);
+
+// Cancel and Confirm button
+const cancelButton = document.getElementById('cancel-button');
+const confirmButton = document.getElementById('confirm-button');
+const popupWindow = document.getElementById('name-edit-popup');
+const popupBackground = document.getElementById('overlay');
+
+// Cancel Button
+function closePopupWindow() {
+    popupWindow.style.display = 'none';
+    popupBackground.style.display = 'none';
 }
+
+// Confirm button
+const nameEditInput = 
+cancelButton.addEventListener('click', closePopupWindow);
