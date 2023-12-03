fb5049a242d151561b8f81cfbf7b8dc8d804643b
hwan-kr <fabronjeon@naver.com>
2023-12-04 08:49:04 +0900

    Lecture 378 complete

--- section-19-starting-project/app.js
+++ section-19-starting-project/app.js
@@ -1,86 +1,31 @@
-const fs = require("fs");
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const uuid = require("uuid");
+const express = require('express');
+
+const defaultRoutes = require('./routes/default');
+const restaurantRoutes = require('./routes/restaurants');
 
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", function (req, res) {
-    res.render("index");
+app.get('/index', function (req, res) {
+    res.render('index');
 });
 
-app.get("/index", function (req, res) {
-    res.render("index");
-});
-
-app.get("/about", function (req, res) {
-    res.render("about");
-});
-
-app.get("/confirm", function (req, res) {
-    res.render("confirm");
-});
-
-app.get("/recommend", function (req, res) {
-    res.render("recommend");
-});
-
-app.post("/recommend", function (req, res) {
-    const restaurant = req.body;
-    restaurant.id = uuid.v4();
-    const filePath = path.join(__dirname, "data", "restaurants.json");
-
-    const fileData = fs.readFileSync(filePath);
-    const storedRestaurants = JSON.parse(fileData);
-
-    storedRestaurants.push(restaurant);
-
-    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
-
-    res.redirect("/confirm");
-});
-
-app.get("/restaurants/:id", function (req, res) {
-    const restaurantId = req.params.id;
-    const filePath = path.join(__dirname, "data", "restaurants.json");
-
-    const fileData = fs.readFileSync(filePath);
-    const storedRestaurants = JSON.parse(fileData);
-
-    for (const restaurant of storedRestaurants) {
-        if (restaurant.id === restaurantId) {
-            return res.render("restaurant-detail", { restaurant: restaurant });
-        }
-    }
-
-    res.status(404).render("404");
-});
-
-app.get("/restaurants", function (req, res) {
-    const filePath = path.join(__dirname, "data", "restaurants.json");
-
-    const fileData = fs.readFileSync(filePath);
-    const storedRestaurants = JSON.parse(fileData);
-
-    res.render("restaurants", {
-        numberOfResrtaurants: storedRestaurants.length,
-        restaurants: storedRestaurants,
-    });
-});
+app.use('/', defaultRoutes);
+app.use('/', restaurantRoutes);
 
 app.use(function (re, res) {
-    res.status(404).render("404");
+    res.status(404).render('404');
 });
 
 app.use(function (error, req, res, next) {
-    res.status(500).render("500");
+    res.status(500).render('500');
 });
 
 app.listen(3000);
